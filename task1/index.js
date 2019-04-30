bae9d871a3fd9631fa834b5147502162b14f25c4
Serhii Melnyk1 <Serhii_Melnyk1@epam.com>
2019-04-30 15:18:05 +0300

    work with ceasar_chipher

--- task1/index.js
+++ task1/index.js
@@ -1,47 +1,43 @@
-function encodeCeasar(text, key) {
-  encodeText = '';
-
-  for (let i = 0; i < text.length; i++) {
-    let code = text.charCodeAt(i);
-    if (code >= 65 && code <= 90) {
-      encodeText += String.fromCharCode(65 + ((code - 65 + key) % 26));
-    } else if (code >= 97 && code <= 122) {
-      encodeText += String.fromCharCode(97 + ((code - 97 + key) % 26));
-    } else {
-      encodeText += String.fromCharCode(code);
-    }
+const ceasar = require('./ceasar');
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    a: 'action',
+    s: 'shift',
+    i: 'input',
+    o: 'output'
   }
+});
 
-  return encodeText;
+const { action, shift, input, output } = argv;
+
+// if (!action || !shift) {
+//   console.error('action or shift is miss');
+//   return 0;
+// }
+
+let text = '';
+if (fs.existsSync(input)) {
+  text = fs.readFileSync(input, 'utf8');
+} else {
+  (async function(){
+    process.stdin.setEncoding('utf8');
+
+    text2 = await process.stdin.on('readable', () => {
+      let text1 = process.stdin.read();
+      process.stdout.write(`data: ${text1}`);
+    });
+
+   return text2
+  })().then(resolve => {console.log(resolve)});
 }
+// console.log(text);
+// outputText(text, output);
 
-function decodeCeasar(encodeText, key) {
-  let decodeText = '';
-
-  for (let i = 0; i < encodeText.length; i++) {
-    let code = encodeText.charCodeAt(i);
-    if (code >= 65 && code <= 90) {
-      let coef = (code - 65 - key) % 26;
-      if (coef >= 0)
-        decodeText += String.fromCharCode(coef + 65);
-      else
-        decodeText += String.fromCharCode(coef + 90)
-    } else if (code >= 97 && code <= 122) { 
-      let coef = (code - 97 - key) % 26;
-      if (coef >= 0)
-        decodeText += String.fromCharCode(coef + 97);
-      else
-        decodeText += String.fromCharCode(coef + 122)
-    } else {
-      decodeText += String.fromCharCode(code);
-    }
+function outputText(text, path) {
+  if (fs.existsSync(path)) {
+    fs.writeFileSync(path, text);
+  } else {
+    process.stdout.write(text);
   }
-
-  return decodeText;
 }
-
-const key = 3;
-let a = encodeCeasar('ABCXYZabcxyz', key);
-let b = decodeCeasar(a, key);
-
-console.log(a, b);
